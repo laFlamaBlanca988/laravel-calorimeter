b6f1e205705badb570ec3fb383df585f7249491f
Sandi Popovic <laflamablanca988@hotmail.com>
2021-09-24 13:58:23 +0200

    Fixing stuff

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -22,9 +22,9 @@
             {{--    USERS TABLE--}}
 
             <div id="users_table" class="users-table-container">
-                <form id="user_search_form" class="search-bar">
-                    <input id="search_input" type="search" value="" name="search" pattern=".*\S.*" required>
-                    <button id="search_button"  class="search-btn" type="submit">
+                <form action="" class="search-bar">
+                    <input id="search_value" type="search" name="search" pattern=".*\S.*" required>
+                    <button class="search-btn" type="submit">
                         <span>Search</span>
                     </button>
                 </form>
@@ -227,5 +227,30 @@
                     </div>
                 </div>
             </div>
+            <script>
+                $(document).ready(function(){
+                    $.ajaxSetup({ cache: false });
+                    $('#search').keyup(function(){
+                        $('#result').html('');
+                        $('#state').val('');
+                        var searchField = $('#search').val();
+                        var expression = new RegExp(searchField, "i");
+                        $.getJSON('data.json', function(data) {
+                            $.each(data, function(key, value){
+                                if (value.name.search(expression) != -1 || value.location.search(expression) != -1)
+                                {
+                                    $('#result').append('<li class="list-group-item link-class"><img src="'+value.image+'" height="40" width="40" class="img-thumbnail" /> '+value.name+' | <span class="text-muted">'+value.location+'</span></li>');
+                                }
+                            });
+                        });
+                    });
+
+                    $('#result').on('click', 'li', function() {
+                        var click_text = $(this).text().split('|');
+                        $('#search').val($.trim(click_text[0]));
+                        $("#result").html('');
+                    });
+                });
+            </script>
 
 @endsection
